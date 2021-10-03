aaa5fefe9a6c4faba3ce850bac365641d0505fe1
yashmks1998 <yashmks1998@gmail.com>
2021-10-04 00:10:34 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,5 @@
+const express =require('express');
+const app=express();
+app.get('/',(req,res)=>res.send('API Running'));
+const port=process.env.PORT || 5000;
+app.listen (port,() => console.log("Server Started on port ${port}"));
